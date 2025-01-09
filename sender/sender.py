43401492a939d58f7df24858baf1f4352e2d5747
DlayelAluhaideb <dlayel.aluhaideb@deeritc.com>
2025-01-09 12:11:23 +0300

    final

--- sender/sender.py
+++ sender/sender.py
@@ -1,7 +1,7 @@
 import requests
 #i will need to create get and post requests
 #in get i need (url,params=meta data for the input)
-r_get = requests.get(url="http://192.168.100.39:8080/data_receiver",params={"Name":"Dlayel"}) #this is the local host of my device that i got from typing ipconfig getifaddr en0 in my terminal
+r_get = requests.get(url="http://localhost:8080/data_receiver",params={"Name":"Dlayel"}) #this is the local host of my device that i got from typing ipconfig getifaddr en0 in my terminal
 print(r_get.text)
-r_post =requests.post(url="http://192.168.100.39:8080/data_receiver",json={"Name":"Shahad"}) #url=https/localhost:port/endpoint
+r_post =requests.post(url="http://localhost:8080/data_receiver",json={"Name":"Shahad"}) #url=https/localhost:port/endpoint
 print(r_post.text)
